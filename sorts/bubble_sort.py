72a1a213334024d86f4e19764eb34600be8bcb56
GKovtonyuk <galyna.kovtonyuk@gmail.com>
2026-04-21 22:41:39 +0300

    add buble_sort

---
+++ sorts/bubble_sort.py
@@ -0,0 +1,7 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            yield j, j + 1, 0, n - i - 1
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
