6ec85fbda84bfa2391d5019d19c74db221cc67c2
GKovtonyuk <112175748+GKovtonyuk@users.noreply.github.com>
2026-04-21 22:46:49 +0300

    Update shell_sort.py

--- sorts/shell_sort.py
+++ sorts/shell_sort.py
@@ -6,13 +6,13 @@ def shell_sort(arr):
             temp = arr[i]
             j = i
             while j >= gap and arr[j - gap] > temp:
-                # 1. Показуємо порівняння та зсув
+               
                 yield j, j - gap, 0, n - 1
                 arr[j] = arr[j - gap]
                 j -= gap
             
             arr[j] = temp
-            # 2. ОБОВ'ЯЗКОВО додаємо yield тут, щоб побачити фінальну вставку temp
+  
             yield j, i, 0, n - 1
             
         gap //= 2
