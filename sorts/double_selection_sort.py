36276f0810c140fdd6dee1b14228b7ac0ef663c1
GKovtonyuk <galyna.kovtonyuk@gmail.com>
2026-04-21 22:44:02 +0300

    add double_selection_sort

---
+++ sorts/double_selection_sort.py
@@ -0,0 +1,21 @@
+def double_selection_sort(arr):
+    left, right = 0, len(arr) - 1
+    while left < right:
+        min_idx, max_idx = left, left
+
+        for i in range(left, right + 1):
+            yield min_idx, max_idx, left, right
+            if arr[i] < arr[min_idx]:
+                min_idx = i
+            if arr[i] > arr[max_idx]:
+                max_idx = i
+
+        arr[left], arr[min_idx] = arr[min_idx], arr[left]
+
+        if max_idx == left:
+            max_idx = min_idx
+
+        arr[right], arr[max_idx] = arr[max_idx], arr[right]
+
+        left += 1
+        right -= 1
