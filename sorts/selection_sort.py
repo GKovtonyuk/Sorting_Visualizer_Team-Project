14c9b902b30f14c82f74f99873c13a9dfea931de
GKovtonyuk <galyna.kovtonyuk@gmail.com>
2026-04-21 22:42:18 +0300

    add selection_sort

---
+++ sorts/selection_sort.py
@@ -0,0 +1,9 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            yield min_idx, j, i, n - 1
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
