0663834c8472f0566fdab327d5e594a2994abc1a
ddosvnua31-arch <ddosvnua31@gmail.com>
2026-05-01 10:20:10 +0300

    Add sort function to inserction

--- sorts/insertion_sort.py
+++ sorts/insertion_sort.py
@@ -1,9 +1,12 @@
+# Murenko Stepan
+# Laba 02
+
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
-        j = i - 1
+        j = i-1
         while j >= 0 and arr[j] > key:
             yield j, i, i, len(arr) - 1
             arr[j + 1] = arr[j]
             j -= 1
-        arr[j + 1] = key
+        arr[j+1] = key
